a679e63832084ca6adac06ca96fc37033eee7d6f
Oliv-Patrik <patrikoli49@gmail.com>
2025-09-13 13:53:32 -0300

    add de operados e declarações

--- tipos_variaveis/numeros.py
+++ tipos_variaveis/numeros.py
@@ -1,9 +1,27 @@
-#Inteiro (Int)
-numero_inteiro = 3
+# Inteiro (Int)
+numero_inteiro = 5
 print("Inteiro = ", numero_inteiro)
 
-#Real com número flutuante (Float)
+# Real com número flutuante (Float)
 numero_real = 3.14
 print("Real com ponto flutuante = ", numero_real)
 
+# Verificar o tipo da VARIAVEL (Type())
+print("Tipo da variavel inteiro = ", type(numero_inteiro))
+print("Tipo da variavel real = ", type(numero_real))
 
+#Soma = +
+soma = 1 + 1
+print("1 + 1 = ", soma)
+
+#Subtração = -
+subtracao = 1 - 1
+print("1 - 1 = ", subtracao)
+
+#Divisão = /
+divisao = 4 / 2
+print("4 / 2 = ", divisao)
+
+#Multiplicação = *
+multiplicacao = 2 * 4
+print("2 x 4 = ", multiplicacao)
---
+++ tipos_variaveis/texto.py
@@ -0,0 +1,11 @@
+# Declaração aspas dupla
+nome_completo = "Patrik Lopes de Oliveira"
+
+# Declaração com 3 aspas para quebras de linha
+nome_completo_aspas = """Patrik
+Lopes 
+de Oliveira"""
+
+# Quebra de linha com \
+nome_completo_quebra = "Patrik \
+    Lopes de Oliveira"
